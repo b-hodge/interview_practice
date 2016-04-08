d7c0c93b92a0c28d4ab3102a38399b377c92f34f
Brian Hodge <brianhodge@utexas.edu>
2016-04-08 13:47:49 -0500

    Added real implementation for reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -1,5 +1,21 @@
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-def reverse_string(s) :
+# HOORAY PYTHON!!
+def reverse_string_slice(s) :
 	return s[::-1]
 
+# But for real
+def reverse_string(s):
+    # convert to list for mutability
+    s = list(s)
+    head_index = 0
+    tail_index = len(s) - 1
+    while head_index < tail_index:
+        # swap needs a temp variable
+        temp = s[head_index]
+        s[head_index] = s[tail_index]
+        s[tail_index] = temp
+        head_index += 1
+        tail_index -= 1
+    return ''.join(s)
+
+print(reverse_string_slice("string"))
 print(reverse_string("string"))
